074a44360a847f9e18923cbb4d99bc0497bebe72
wobsoriano <sorianorobertc@gmail.com>
2022-12-17 15:30:45 -0800

    reinstall stuff

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
   serverDependenciesToBundle: [/@auth\/core/, 'oauth4webapi', 'preact-render-to-string']
   // appDirectory: "app",
